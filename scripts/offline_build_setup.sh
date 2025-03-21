09e7b6b8ed4b239f51bebb29666837a8d4ce45d1
flowjiyun <gyp2795@gmail.com>
2025-03-21 10:08:23 +0900

    typo fix

--- scripts/offline_build_setup.sh
+++ scripts/offline_build_setup.sh
@@ -24,8 +24,8 @@ cat << 'EOF' >> "$HOME/.bashrc"
 . $HOME/.cargo/env
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 EOF
 
 # Install vbox extension pack
